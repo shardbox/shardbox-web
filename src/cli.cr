ea8456676969775f93e5372a1394710d3eeb3d1b
Johannes Müller <straightshoota@gmail.com>
2020-04-07 14:50:49 +0200

    Fix mutex lock on initializaion

--- src/cli.cr
+++ src/cli.cr
@@ -1,3 +1,9 @@
 require "./app"
 
+# Explicitly initialize a connection before listening to HTTP requests in order
+# to avoid deadlocks on setup_connection.
+ShardsDB.connect do |db|
+  puts db.connection.scalar("SELECT MAX(version) FROM schema_migrations").as(String)
+end
+
 Kemal.run
